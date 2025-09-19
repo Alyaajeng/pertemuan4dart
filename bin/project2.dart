46cf5c3f7ea297145fd6592cf9725da7391bdd80
Alyaajeng <alyaajeng29@gmail.com>
2025-09-19 11:22:58 +0700

    fixedList

---
+++ bin/project2.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:project2/project2.dart' as project2;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${project2.calculate()}!');
+
+  var fl = List<int>.filled(4, 0);
+  fl[0] = 1;
+  fl[1] = 2;
+  fl[2] = 3;
+  fl[3] = 4;
+  // f1[4] = 5;
+  //cetak
+  stdout.writeln(fl);
+  //List<int> angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
